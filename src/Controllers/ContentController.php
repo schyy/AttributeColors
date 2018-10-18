0eba7a6c0a0232e9f93b2d106b1df8275f972846
schyy <b.kosinski@azupedia.com>
2018-10-18 13:02:42 +0200

    0.2.10

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -7,7 +7,7 @@ use Plenty\Plugin\Controller;
 use Plenty\Plugin\Containers;
 use Plenty\Plugin\Templates\Twig;
 
-use Plenty\Modules\Item\Variation\Contracts
+use Plenty\Modules\Item\Variation\Contracts;
 
 class ContentController extends Controller
 {
