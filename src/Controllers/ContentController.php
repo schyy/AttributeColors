6537bf88e3626f55f9ffabb6d986503e4b2dc169
schyy <b.kosinski@azupedia.com>
2018-10-18 17:10:56 +0200

    0.2.16

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -7,8 +7,6 @@ use Plenty\Plugin\Controller;
 use Plenty\Plugin\Containers;
 use Plenty\Plugin\Templates\Twig;
 
-use Plenty\Modules\Item\Variation\Contracts;
-
 class ContentController extends Controller
 {
     public function showColors(Twig $twig):string
