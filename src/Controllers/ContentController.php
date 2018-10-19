e78f2e546de3d586f4b7b21b0184c7a01bdeb9c0
schyy <b.kosinski@azupedia.com>
2018-10-19 15:21:52 +0200

    0.2.28

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -6,11 +6,13 @@ namespace AttributeColors\Controllers;
 use Plenty\Plugin\Controller;
 use Plenty\Plugin\Containers;
 use Plenty\Plugin\Templates\Twig;
+use AttributeColors\Extensions\AttributeColorsExtension;
 
 class ContentController extends Controller
 {
     public function showColors(Twig $twig):string
     {
+        $twig->addExtension(AttributeColorsExtension::class);
         return $twig->render('AttributeColors::content.colors');
     }
 }
